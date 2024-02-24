b4ba3cb4634bee56fe212f95c49593783a848f1c
KevinJPF <kevinjuliano.pires@gmail.com>
2024-02-24 20:56:20 -0300

    feat: player class object

---
+++ src/classes/Player.java
@@ -0,0 +1,36 @@
+package classes;
+
+public class Player {
+    String _playerName;
+
+    public String getPlayerName() {
+        return _playerName;
+    }
+
+    public void setPlayerName(String _playerName) {
+        this._playerName = _playerName;
+    }
+
+    Integer _playerPoints;
+
+    public Integer getPlayerPoints() {
+        return _playerPoints;
+    }
+
+    Board _playerBoard;
+
+    public Board getPlayerBoard() {
+        return _playerBoard;
+    }
+
+    public Player(String playerName, Board playerBoard) {
+        _playerName = playerName;
+        _playerBoard = playerBoard;
+        _playerPoints = 0;
+    }
+
+    public Integer addPoint() {
+        _playerPoints++;
+        return _playerPoints;
+    }
+}
