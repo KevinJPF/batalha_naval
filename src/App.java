9f5d89ec9e0515a457b807e0efdfe729a9cca9e1
KevinJPF <kevinjuliano.pires@gmail.com>
2024-02-25 03:39:56 -0300

    feat: tested all objects, parameters and methods

--- src/App.java
+++ src/App.java
@@ -1,46 +1,31 @@
 import classes.Board;
+import classes.Player;
 import classes.Ship;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Board firstBoard = new Board(7, 10);
+        Player firstPlayer = new Player("Player1", firstBoard);
 
+        System.out.println("Adicionando navios: ");
         firstBoard.addShip("Ship1", 4, 8);
         firstBoard.addShip("Ship2", 8, 3);
         firstBoard.addShip("Ship3", 1, 2);
         firstBoard.addShip("Ship4", 6, 12);
 
-        System.out.println("O tabuleiro 1 tem os navios: ");
+        System.out.println("\nListagem de navios no tabuleiro: ");
         for (Ship ship : firstBoard.getShips()) {
             System.out.println(ship.getShipName());
         }
 
-        boolean[][] firstBoardCoordinates = firstBoard.getBoardCoordinates();
-        String rowToPrint = "|";
-        char[] alfabeto = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        System.out.println("\nAtirando no tabuleiro: ");
+        System.out.println(firstBoard.shootBoard(2, 4));
+        System.out.println(firstBoard.shootBoard(8, 1));
+        System.out.println(firstBoard.shootBoard(4, 8));
+        System.out.println(firstBoard.shootBoard(5, 11));
 
-        System.out.println("Tabuleiro 1:");
-        for (int i = -1; i < firstBoardCoordinates.length; i++) {
-            System.out.println("______________________");
-            for (int j = -1; j < firstBoardCoordinates[0].length; j++) {
-                if (i < 0) {
-                    if (j < 0) {
-                        rowToPrint += "*|";
-                    } else {
-                        rowToPrint += alfabeto[j] + "|";
-                    }
-                } else {
-                    if (j < 0) {
-                        rowToPrint += (i + 1) + "|";
-                    } else {
-                        rowToPrint += (firstBoardCoordinates[i][j] ? "X" : "O") + "|";
-                    }
-                }
-            }
-            System.out.println(rowToPrint);
-            rowToPrint = "|";
-        }
 
-        System.out.println("Batalha Naval!");
+        System.out.println("\nTabuleiro 1:");
+        System.out.println(firstPlayer.getPlayerBoard().printBoard());
     }
 }
